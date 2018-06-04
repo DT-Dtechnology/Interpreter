7a66ed1705ccde2c7fd500945abb4bf127f921fa
DT-Dtechnology <DT-Dtechnology@outlook.com>
2018-06-04 14:39:01 +0800

    DongHaowen

--- Interpreter/Block.cpp
+++ Interpreter/Block.cpp
@@ -7,7 +7,7 @@ void Block::print_all()
 	for (auto it = sentence_vector_->begin(); it != sentence_vector_->end(); ++it)
 	{
 		SentenceParser* sp = new SentenceParser((*it));
-		sp->print_test_first();
+		sp->print_test_second();
 		delete sp;
 	}
 }
