e4963cb065dae12aaa5609419dbd4124c318f33a
都头 武 <chenxy_1@yeah.net>
2018-06-06 01:22:47 +0800

    m

--- Interpreter/Matrix.h
+++ Interpreter/Matrix.h
@@ -7,8 +7,8 @@
 using std::string;
 using std::map;
 
-const int MatrixRow = 7;
-const int MatrixColumn = 10;
+const int MatrixRow = 23;
+const int MatrixColumn = 31;
 
 //####
 bool isUnTerminate[256];
@@ -16,7 +16,7 @@ void buildIsUnTerminate()
 {
 	for (int i = 0; i < 256; i++)
 		isUnTerminate[i] = false;
-	char UTsets[] = { 'A', 'L', 'E', 'T', 'G', 'H', 'F' };
+	char UTsets[] = { 'E', 'E' - 64, 'T', 'T' - 64, 'F', 'F' - 64, 'G', 'G' - 64, 'H', 'S' - 64, 'A', 'L', 'W', 'W' - 64, 'D', 'Q', 'S', 'P', 'V' - 64, 'R', 'I', 'J', 'P' - 64 };
 		for (auto i : UTsets) {
 			isUnTerminate[(int)i] = true;
 		}
@@ -28,7 +28,7 @@ void buildIsTerminate()
 	for (int i = 0; i < 256; i++)
 		isUnTerminate[i] = false;
 	//= ~ ( ) b c + - * / 
-	char Tsets[] = { '=', '(', ')', '+', '-', '*', '/' , '#'};
+	char Tsets[] = { 'i', 'a', 'b', '#', 'c', 'd', '+' , '-', '*', '/', '(', ')', '=', 'l', ':', 'n', 'o', ',', 'q', '!', 'r', 's', 't', 'u', 'v', 'w', 'e', 'x', 'y', 'z', 'f' };
 	for (auto i : Tsets) {
 		isTerminate[(int)i] = true;
 	}
@@ -63,13 +63,19 @@ string Matrix[MatrixRow][MatrixColumn];
 map<string, char> stringToChar;
 void buildStoC()
 {
-	string UTsets[] = { "A", "L", "E", "T", "G", "H", "F" };
-	char UTsetc[] = { 'A', 'L', 'E', 'T', 'G', 'H', 'F' };
+	char UTsetc[] = { 'E', 'E' - 64, 'T', 'T' - 64, 'F', 'F' - 64, 'G', 'G' - 64, 'H', 'S' - 64, 'A', 'L', 'W', 'W' - 64, 'D', 'Q', 'S', 'P', 'V' - 64, 'R', 'I', 'J', 'P' - 64 };
+	string UTsets[MatrixRow];
+	for (int i = 0; i < MatrixRow; ++i ) {
+		UTsets[i] = UTsetc[i];
+	}
 	for (int i = 0; i < MatrixRow; i++) {
 		stringToChar[UTsets[i]] = UTsetc[i];
-		}
-	string Tsets[] = { "=", "#", "(", ")", "b", "c", "+", "-", "*", "/" };
-	char Tsetc[] = { '=', '#', '(', ')', 'b', 'c', '+', '-', '*', '/' };
+	}
+	char Tsetc[] = { 'i', 'a', 'b', '#', 'c', 'd', '+' , '-', '*', '/', '(', ')', '=', 'l', ':', 'n', 'o', ',', 'q', '!', 'r', 's', 't', 'u', 'v', 'w', 'e', 'x', 'y', 'z', 'f' };
+	string Tsets[MatrixColumn];
+	for (int i = 0; i < MatrixColumn; ++i) {
+		Tsets[i] = Tsetc[i];
+	}
 	for (int i = 0; i < MatrixColumn; i++) {
 		stringToChar[Tsets[i]] = Tsetc[i];
 	}
@@ -80,13 +86,29 @@ void buildStoC()
 map<NodeType, int> nodeToInt;
 void buildNtoI()
 {
-	nodeToInt[TEST_A] = 0;
-	nodeToInt[TEST_E] = 1;
-	nodeToInt[TEST_G] = 2;
-	nodeToInt[TEST_T] = 3;
-	nodeToInt[TEST_H] = 4;
-	nodeToInt[TEST_F] = 5;
-	nodeToInt[TEST_L] = 6;
+	nodeToInt[EXP] = 0;
+	nodeToInt[EE] = 1;
+	nodeToInt[T] = 2;
+	nodeToInt[TT] = 3;
+	nodeToInt[F] = 4;
+	nodeToInt[FF] = 5;
+	nodeToInt[G] = 6;
+	nodeToInt[GG] = 7;
+	nodeToInt[H] = 8;
+	nodeToInt[SEN] = 9;
+	nodeToInt[ASS] = 10;
+	nodeToInt[LOOP] = 11;
+	nodeToInt[EXPL] = 12;
+	nodeToInt[EXPLL] = 13;
+	nodeToInt[DEF] = 14;
+	nodeToInt[FUNC] = 15;
+	nodeToInt[SUF] = 16;
+	nodeToInt[PRO] = 17;
+	nodeToInt[VARL] = 18;
+	nodeToInt[VARLL] = 19;
+	nodeToInt[IF] = 20;
+	nodeToInt[JUMP] = 21;
+	nodeToInt[PRINT] = 22;
 }
 
 //map<int, NodeType> intToNode;
@@ -106,15 +128,32 @@ map<char, NodeType> charToNode;
 void buildCtoN()
 {
 	//'A', 'L', 'E', 'T', 'G', 'H', 'F', 'P', 'M'
-	charToNode['A'] = TEST_A;
-	charToNode['L'] = TEST_L;
-	charToNode['E'] = TEST_E;
-	charToNode['T'] = TEST_T;
-	charToNode['G'] = TEST_G;
-	charToNode['H'] = TEST_H;
-	charToNode['F'] = TEST_F;
-	charToNode['P'] = TEST_P;
-	charToNode['M'] = TEST_M;
+	charToNode['E'] = EXP;
+	charToNode['E' - 64] = EE;
+	charToNode['T'] = T;
+	charToNode['T' - 64] = TT;
+	charToNode['F'] = F;
+	charToNode['F' - 64] = FF;
+	charToNode['G'] = G;
+	charToNode['G' - 64] = GG;
+	charToNode['H'] = H;
+	charToNode['Q'] = FUNC;
+	charToNode['S' - 64] = SEN;
+	charToNode['L'] = LOOP;
+	charToNode['I'] = IF;
+	charToNode['J'] = JUMP;
+	charToNode['D'] = DEF;
+	charToNode['P' - 64] = PRINT;
+	charToNode['A'] = ASS;
+	charToNode['W'] = EXPL;
+	charToNode['W' - 64] = EXPLL;
+	charToNode['S'] = SUF;
+	charToNode['P'] = PRO;
+	charToNode['V'-64] = VARL;
+	charToNode['R'] = VARLL;
+	charToNode['I'] = IF;
+	charToNode['J'] = JUMP;
+	charToNode['P' - 64] = PRINT;
 	//'=', '#', '(', ')', 'b', 'c', '+', '-', '*', '/'
 	charToNode['='] = EQUAL;
 	charToNode['('] = LEFT_BRACKET;
@@ -123,6 +162,29 @@ void buildCtoN()
 	charToNode['-'] = MINUS;
 	charToNode['*'] = MULTIPLY;
 	charToNode['/'] = DIVIDE;
+	charToNode['a'] = IS_EQUAL;
+	charToNode['b'] = IS_NOT_EQUAL;
+	charToNode['c'] = LEFT_MOVE;
+	charToNode['d'] = RIGHT_MOVE;
+	charToNode['l'] = WHILE;
+	charToNode['n'] = FOR;
+	charToNode['o'] = IN;
+	charToNode[','] = COMMA;
+	charToNode[':'] = COLON;
+	charToNode['q'] = DEF;
+	charToNode['!'] = BANG;
+	charToNode['r'] = NOT;
+	charToNode['s'] = PLUS_PLUS;
+	charToNode['t'] = MINUS_MINUS;
+	charToNode['u'] = IF;
+	charToNode['v'] = ELIF;
+	charToNode['w'] = ELSE;
+	charToNode['x'] = CONTINUE;
+	charToNode['y'] = BREAK;
+	charToNode['z'] = PASS;
+	charToNode['e'] = RETURN;
+	charToNode['f'] = PRINT;
+	charToNode['i'] = VARIABLE;
 	charToNode['#'] = END;
 }
 void buildMatrix();
@@ -131,16 +193,37 @@ void buildMatrix();
 map<string, int> stringToInt;
 void buildStoI()
 {
-	stringToInt["="] = 0;
-	stringToInt["+"] = 1;
-	stringToInt["-"] = 2;
+	stringToInt["variable"] = 0;
+	stringToInt["=="] = 1;
+	stringToInt["!="] = 2;
 	stringToInt["#"] = 3;
-	stringToInt["*"] = 4;
-	stringToInt["/"] = 5;
-	stringToInt["("] = 6;
-	stringToInt[")"] = 7;
-	stringToInt["b"] = 8;
-	stringToInt["c"] = 9;
+	stringToInt["<<"] = 4;
+	stringToInt[">>"] = 5;
+	stringToInt["+"] = 6;
+	stringToInt["-"] = 7;
+	stringToInt["*"] = 8;
+	stringToInt["/"] = 9;
+	stringToInt["("] = 10;
+	stringToInt[")"] = 11;
+	stringToInt["="] = 12;
+	stringToInt["while"] = 13;
+	stringToInt[":"] = 14;
+	stringToInt["for"] = 15;
+	stringToInt["in"] = 16;
+	stringToInt[","] = 17;
+	stringToInt["def"] = 18;
+	stringToInt["!"] = 19;
+	stringToInt["not"] = 20;
+	stringToInt["++"] = 21;
+	stringToInt["--"] = 22;
+	stringToInt["if"] = 23;
+	stringToInt["elif"] = 24;
+	stringToInt["else"] = 25;
+	stringToInt["return"] = 26;
+	stringToInt["continue"] = 27;
+	stringToInt["break"] = 28;
+	stringToInt["pass"] = 29;
+	stringToInt["print"] = 30;
 }
 //####
 
@@ -174,6 +257,11 @@ void buildMatrix()
 		{
 			row++;
 			column = 0;
+			continue;
+		}
+		if (c == 39)
+		{
+			str[str.length() - 1] -= 64;
 			continue;
 		}
 		if (c == '|')
