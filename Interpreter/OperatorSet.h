5b67a9d2fce22bc66dbcd43832f21cb22086d86e
DT-Dtechnology <DT-Dtechnology@outlook.com>
2018-06-11 22:51:12 +0800

    DongHaowen

--- Interpreter/OperatorSet.h
+++ Interpreter/OperatorSet.h
@@ -8,13 +8,13 @@ using std::string;
 string python_operator[] =
 {
 	"+", "-", "*", "/", "%", "&", "|", "=",
-	"**", "^", "~", "//", "[", "]", ":",
+	"**", "^", "~", "//", "[", "]",
 	"<<", ">>", "<=", "==", "!=", ">=",
 	"<", ">", "%=", "<>", "/=","-=",
 	"+=", "*=", "(", ")", ","
 };
 
-set<string> operatorSet(python_operator, python_operator + 32);
+set<string> operatorSet(python_operator, python_operator + 31);
 
 
 string python_keyword[] =
@@ -22,9 +22,9 @@ string python_keyword[] =
 "return",  "elif",   "in",     "try",     "and",   "else",
 "is",      "while",    "as",     "except", "lambda",  "with",  "assert",
 "finally", "nonlocal", "yield",  "break",  "for",     "class", "from",
-"or",      "continue", "global", "pass", "not", "print"
+"or",      "continue", "global", "pass", "not", "print", ":"
 };
 
 
 
-set<string> KeyWordSet(python_keyword, python_keyword + 32);
+set<string> KeyWordSet(python_keyword, python_keyword + 33);
